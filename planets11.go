9d16da62bcb443009279fa12e55cdd11dd56bf80
RR <9381804@gmail.com>
2025-02-03 00:19:36 +0100

    creating base composition

--- planets11.go
+++ planets11.go
@@ -1,6 +1,10 @@
 package planets11
 
-import "github.com/RulezKT/floatsfile"
+import (
+	"math"
+
+	"github.com/RulezKT/floatsfile"
+)
 
 const (
 	MOON_FILE       = "moon.bin"
@@ -40,6 +44,7 @@ type Pl11 struct {
 
 func (pl *Pl11) Load(dir string) {
 
+	pl.earth = floatsfile.LoadBinary(dir+EARTH_FILE, 752801)
 	pl.moon = floatsfile.LoadBinary(dir+MOON_FILE, 752801)
 	pl.sun = floatsfile.LoadBinary(dir+SUN_FILE, 160685)
 	pl.mer = floatsfile.LoadBinary(dir+MER_FILE, 403788)
@@ -50,11 +55,77 @@ func (pl *Pl11) Load(dir string) {
 	pl.ura = floatsfile.LoadBinary(dir+URA_FILE, 45900)
 	pl.nep = floatsfile.LoadBinary(dir+NEP_FILE, 45900)
 	pl.plu = floatsfile.LoadBinary(dir+PLU_FILE, 45900)
-	pl.earth = floatsfile.LoadBinary(dir+EARTH_FILE, 752801)
 	pl.earthb = floatsfile.LoadBinary(dir+EARTH_BARY_FILE, 188149)
 }
 
 func (pl *Pl11) Calc(seconds int64) []Position {
-	return []Position{}
+	return []Position{
+		// earth
+		calcPos11(seconds, -3_157_963_200, 41, 345_600, 12, pl.earth),
 
+		// moon
+		calcPos11(seconds, -3_157_963_200, 41, 345_600, 12, pl.moon),
+
+		// sun
+		calcPos11(seconds, -3_157_444_800, 35, 1_382_400, 10, pl.sun),
+
+		// mercury
+		calcPos11(seconds, -3_155_716_800, 44, 691_200, 13, pl.mer),
+
+		// venus
+		calcPos11(seconds, -3_156_062_400, 32, 1_382_400, 9, pl.ven),
+
+		// mars
+		calcPos11(seconds, -3_156_753_600, 35, 2_764_800, 10, pl.mar),
+
+		// jupyter
+		calcPos11(seconds, -3_156_753_600, 26, 2_764_800, 7, pl.jup),
+
+		// saturn
+		calcPos11(seconds, -3_156_753_600, 23, 2_764_800, 6, pl.sat),
+
+		// uranus
+		calcPos11(seconds, -3_156_753_600, 20, 2_764_800, 5, pl.ura),
+
+		// neptune
+		calcPos11(seconds, -3_156_753_600, 20, 2_764_800, 5, pl.nep),
+
+		// pluto
+		calcPos11(seconds, -3_156_753_600, 20, 2_764_800, 5, pl.plu),
+
+		// earth barycenter
+		calcPos11(seconds, -3_156_062_400, 41, 1_382_400, 12, pl.earthb),
+	}
+
+}
+
+func calcPos11(seconds int64, startTime int64, rsize int, intlen int, order int, arrPtr []float64) Position {
+
+	deg := order + 1
+
+	offset := math.Floor((float64(seconds)-float64(startTime))/float64(intlen)) * float64(rsize)
+	data := arrPtr[int64(offset) : int64(offset)+int64(rsize)]
+	tau := (float64(seconds) - data[0]) / data[1]
+
+	return Position{
+		X: chebyshev(order, tau, data[2:2+deg]),
+		Y: chebyshev(order, tau, data[2+deg:2+2*deg]),
+		Z: chebyshev(order, tau, data[2+2*deg:2+3*deg]),
+	}
+}
+
+func chebyshev(order int, x float64, data []float64) float64 {
+
+	// Evaluate a Chebyshev polynomial
+	var bk float64
+	two_x := 2 * x
+	bkp2 := data[order]
+	bkp1 := two_x*bkp2 + data[order-1]
+
+	for n := order - 2; n > 0; n-- {
+		bk = data[n] + two_x*bkp1 - bkp2
+		bkp2 = bkp1
+		bkp1 = bk
+	}
+	return data[0] + x*bkp1 - bkp2
 }
