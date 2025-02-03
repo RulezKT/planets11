955b6e7161ed24422f6ad76dde04ba238bf1fa92
RR <9381804@gmail.com>
2025-02-03 01:21:35 +0100

    further refactoring

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/RulezKT/planets11
 
 go 1.22.2
 
-require github.com/RulezKT/floatsfile v0.0.0-20250202191914-51af1fdd3adc // indirect
+require (
+	github.com/RulezKT/floatsfile v0.0.0-20250202191914-51af1fdd3adc // indirect
+	github.com/RulezKT/types v0.0.0-20250117165219-e38f9be1beac // indirect
+)
--- planets11.go
+++ planets11.go
@@ -2,8 +2,10 @@ package planets11
 
 import (
 	"math"
+	"path/filepath"
 
 	"github.com/RulezKT/floatsfile"
+	"github.com/RulezKT/types"
 )
 
 const (
@@ -19,13 +21,20 @@ const (
 	PLU_FILE        = "pluto.bin"
 	EARTH_FILE      = "earth.bin"
 	EARTH_BARY_FILE = "earth_bary.bin"
-)
 
-type Position struct {
-	X float64
-	Y float64
-	Z float64
-}
+	EARTH_LENGTH      = 752801
+	MOON_LENGTH       = 752801
+	SUN_LENGTH        = 160685
+	MER_LENGTH        = 403788
+	VEN_LENGTH        = 146848
+	MAR_LENGTH        = 80325
+	JUP_LENGTH        = 59670
+	SAT_LENGTH        = 52785
+	URA_LENGTH        = 45900
+	NEP_LENGTH        = 45900
+	PLU_LENGTH        = 45900
+	EARTH_BARY_LENGTH = 188149
+)
 
 type Pl11 struct {
 	moon   []float64
@@ -44,22 +53,22 @@ type Pl11 struct {
 
 func (pl *Pl11) Load(dir string) {
 
-	pl.earth = floatsfile.LoadBinary(dir+EARTH_FILE, 752801)
-	pl.moon = floatsfile.LoadBinary(dir+MOON_FILE, 752801)
-	pl.sun = floatsfile.LoadBinary(dir+SUN_FILE, 160685)
-	pl.mer = floatsfile.LoadBinary(dir+MER_FILE, 403788)
-	pl.ven = floatsfile.LoadBinary(dir+VEN_FILE, 146848)
-	pl.mar = floatsfile.LoadBinary(dir+MAR_FILE, 80325)
-	pl.jup = floatsfile.LoadBinary(dir+JUP_FILE, 59670)
-	pl.sat = floatsfile.LoadBinary(dir+SAT_FILE, 52785)
-	pl.ura = floatsfile.LoadBinary(dir+URA_FILE, 45900)
-	pl.nep = floatsfile.LoadBinary(dir+NEP_FILE, 45900)
-	pl.plu = floatsfile.LoadBinary(dir+PLU_FILE, 45900)
-	pl.earthb = floatsfile.LoadBinary(dir+EARTH_BARY_FILE, 188149)
+	pl.earth = floatsfile.LoadBinary(filepath.Join(dir, EARTH_FILE), EARTH_LENGTH)
+	pl.moon = floatsfile.LoadBinary(filepath.Join(dir, MOON_FILE), MOON_LENGTH)
+	pl.sun = floatsfile.LoadBinary(filepath.Join(dir, SUN_FILE), SUN_LENGTH)
+	pl.mer = floatsfile.LoadBinary(filepath.Join(dir, MER_FILE), MER_LENGTH)
+	pl.ven = floatsfile.LoadBinary(filepath.Join(dir, VEN_FILE), VEN_LENGTH)
+	pl.mar = floatsfile.LoadBinary(filepath.Join(dir, MAR_FILE), MAR_LENGTH)
+	pl.jup = floatsfile.LoadBinary(filepath.Join(dir, JUP_FILE), JUP_LENGTH)
+	pl.sat = floatsfile.LoadBinary(filepath.Join(dir, SAT_FILE), SAT_LENGTH)
+	pl.ura = floatsfile.LoadBinary(filepath.Join(dir, URA_FILE), URA_LENGTH)
+	pl.nep = floatsfile.LoadBinary(filepath.Join(dir, NEP_FILE), NEP_LENGTH)
+	pl.plu = floatsfile.LoadBinary(filepath.Join(dir, PLU_FILE), PLU_LENGTH)
+	pl.earthb = floatsfile.LoadBinary(filepath.Join(dir, EARTH_BARY_FILE), EARTH_BARY_LENGTH)
 }
 
-func (pl *Pl11) Calc(seconds int64) []Position {
-	return []Position{
+func (pl *Pl11) Calc(seconds float64) []types.Position {
+	return []types.Position{
 		// earth
 		calcPos11(seconds, -3_157_963_200, 41, 345_600, 12, pl.earth),
 
@@ -99,15 +108,15 @@ func (pl *Pl11) Calc(seconds int64) []Position {
 
 }
 
-func calcPos11(seconds int64, startTime int64, rsize int, intlen int, order int, arrPtr []float64) Position {
+func calcPos11(seconds float64, startTime int64, rsize int, intlen int, order int, arrPtr []float64) types.Position {
 
 	deg := order + 1
 
-	offset := math.Floor((float64(seconds)-float64(startTime))/float64(intlen)) * float64(rsize)
+	offset := math.Floor((seconds-float64(startTime))/float64(intlen)) * float64(rsize)
 	data := arrPtr[int64(offset) : int64(offset)+int64(rsize)]
-	tau := (float64(seconds) - data[0]) / data[1]
+	tau := (seconds - data[0]) / data[1]
 
-	return Position{
+	return types.Position{
 		X: chebyshev(order, tau, data[2:2+deg]),
 		Y: chebyshev(order, tau, data[2+deg:2+2*deg]),
 		Z: chebyshev(order, tau, data[2+2*deg:2+3*deg]),
